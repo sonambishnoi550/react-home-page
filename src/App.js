8b06eadf4ae190064a0b29433aa17cb1dfa288fb
sonambishnoi550 <bishnoisonam079@gmail.com>
2024-11-25 14:08:24 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Hero/>
+      <About/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>
+      <h1>hlo world</h1>
+    </div>
+  )
+}
+
+export default Hero
